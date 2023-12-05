c9543e3957b2de580928ecb1b01a56973fe1cc93
kingseungwooya <ksw_0505@nate.com>
2023-12-05 13:49:54 +0900

    비 객체 제작

---
+++ Project1/Rain.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+class Rain {
+public:
+	// ¼ø¹ø
+	int index = 0;
+	// ÁÂÇ¥ 
+	int x = 0;
+	int y = 0;
+
+};
